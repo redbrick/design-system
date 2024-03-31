1eea70500d2f8cce631905695cf45919cc6173bd
nova <novanai.dev@gmail.com>
2024-03-31 23:20:21 +0100

    Add nix theme

--- tailwind/index.js
+++ tailwind/index.js
@@ -6,27 +6,50 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        light: {
+        'nix-light': {
           ...themes.light,
-          primary: '#bc171c',
-          secondary: '#1762d2',
-          accent: '#b41a3b',
+          primary: '#5277c3',
+          secondary: '#6ad541',
+          accent: '#27385d',
+          neutral: '#7ebae4',
+          'primary-content': '#ffffff',
+          'secondary-content': '#ffffff',
+          'neutral-content': '#ffffff',
         },
-      },
-      {
-        dark: {
+        'nix-dark': {
           ...themes.dark,
-          primary: '#dc2b31',
-          secondary: '#1784d2',
-          accent: '#d21746',
-        },
+          primary: '#5277c3',
+          secondary: '#6ad541',
+          accent: '#27385d',
+          neutral: '#7ebae4',
+          'primary-content': '#ffffff',
+          'secondary-content': '#ffffff',
+          'neutral-content': '#ffffff',
+        }
       },
+      // {
+      //   light: {
+      //     ...themes.light,
+      //     primary: '#bc171c',
+      //     secondary: '#1762d2',
+      //     accent: '#b41a3b',
+      //   },
+      // },
+      // {
+      //   dark: {
+      //     ...themes.dark,
+      //     primary: '#dc2b31',
+      //     secondary: '#1784d2',
+      //     accent: '#d21746',
+      //   },
+      // },
       {
         synthwave: {
           ...themes.synthwave,
           primary: '#cd1946',
         },
       },
+      
     ],
   },
 }
